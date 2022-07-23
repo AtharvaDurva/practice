4927b2007ddeb2d774992910ed09a00e743eb03d
Yatharth136 <yatharth136@gmail.com>
2022-07-23 12:10:01 -0400

    file added and line added

---
+++ practice.java
@@ -0,0 +1,13 @@
+package github;
+
+public class practice {
+    public static void main(String[] args) {
+        int a = 70;
+        int b = 10;
+
+        System.out.println(a+b);
+System.out.println(a-b);
+
+
+    }
+}
